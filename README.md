ad2e1cb372c7278b287b31735d7d71fac561cf76
dollarkillerx <adapawang@gmail.com>
2021-02-16 22:18:05 +0800

    udp api

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # baostock
-baostock 数据同步本地
+Open-Source-Data 数据同步本地
 
 # install
 ``` 
@@ -16,6 +16,30 @@ make debug
 
 ### Baostock Server
 - [x] K 线基础数据
+
+### 数据来源
+- Baostock
+- Akshare
+
+### history stocks 获取历史数据
+POST: `/history_stocks`
+```json
+{
+  "id": "sh.601398",
+  "start_time": "2021-01-01",
+  "end_time": "2021-02-16",
+  "type": 0,
+  "restoration": 1
+} 
+```
+
+### history fund 获取基金历史净值
+GET: `history_fund/:fund_id`
+
+### TOP100 funds rank
+GET: `/funds_rank`
+
+
 
 ### 依赖
 - fast_api
--- src/main.py
+++ src/main.py
@@ -2,25 +2,34 @@ from fastapi import FastAPI, Response
 from pydantic import BaseModel
 import baostock as bs
 import src.option as option
+import akshare as ak
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return "baostock Api Server"
+async def read_root():
+    return "Open-Source-Data  Api Server"
 
 
 class StockInfo(BaseModel):
-    id: str
-    start_time: str
-    end_time: str
+    id: str  # 股票代码
+    start_time: str  # 格式“YYYY-MM-DD”，为空时取2015-01-01；
+    end_time: str  # 格式“YYYY-MM-DD”，为空时取最近一个交易日；
     type: int  # 0 日key 1 周k  2 1小时k  3 30分钟K
     restoration: int  # 0 原始 1 前复权 2 后复权
+# {
+#     "id": "sh.601398",
+#     "start_time": "2021-01-01",
+#     "end_time": "2021-02-16",
+#     "type": 0,
+#     "restoration": 1
+# }
 
 
+# 获取stocks历史数据
 @app.post("/history_stocks", status_code=200)
-def history_stocks(stock_info: StockInfo, response: Response):
+async def history_stocks(stock_info: StockInfo, response: Response):
     try:
         bs.login()
         frequency = "d"
@@ -29,7 +38,8 @@ def history_stocks(stock_info: StockInfo, response: Response):
 
         if stock_info.type == 0:
             frequency = "d"
-            query = "date,code,open,high,low,close,preclose,volume,amount,adjustflag,turn,tradestatus,pctChg,peTTM,pbMRQ,psTTM,pcfNcfTTM,isST"
+            query = "date,code,open,high,low,close,preclose,volume,amount,adjustflag,turn,tradestatus,pctChg,peTTM," \
+                    "pbMRQ,psTTM,pcfNcfTTM,isST"
         elif stock_info.type == 1:
             frequency = "w"
             query = "date,code,open,high,low,close,volume,amount,adjustflag,turn,pctChg"
@@ -63,3 +73,45 @@ def history_stocks(stock_info: StockInfo, response: Response):
     except Exception as e:
         response.status_code = 500
         return option.standard_return(None, False, e, query)
+
+
+# 获取fund历史数据
+@app.get("/history_fund/{fund_id}", status_code=200)
+async def history_fund(fund_id: str, response: Response):
+    try:
+        r = ak.fund_em_open_fund_info(fund=fund_id)
+        return r
+    except Exception as e:
+        response.status_code = 400
+        return e
+
+
+# fund排名
+@app.get("/funds_rank", status_code=200)
+async def funds_rank(response: Response):
+    try:
+        fund_em_open_fund_rank_df = ak.fund_em_open_fund_rank(symbol="全部")
+        # print(fund_em_open_fund_rank_df.columns)
+        # print(fund_em_open_fund_rank_df.aggregate)
+
+        resp = []
+        idx = 0
+        for i in fund_em_open_fund_rank_df['基金代码']:
+            resp.append({"fund_id": i})
+            idx += 1
+            if idx >= 100:
+                break
+
+        idx = 0
+        for i in fund_em_open_fund_rank_df['基金简称']:
+            resp[idx]['fund_name'] = i
+            idx += 1
+            if idx >= 100:
+                break
+
+        return resp
+    except Exception as e:
+        response.status_code = 400
+        return e
+
+# fund 持仓信息
